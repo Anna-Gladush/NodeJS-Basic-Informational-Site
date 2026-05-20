e7b038fb36e81a3ade6babff3795cdcf89bb2a6b
Anna-Gladush <ann.gladush@gmail.com>
2026-05-20 15:57:33 +0200

    added simple content to html files (including navigation), created server and learned how to work with http, path and fs modules

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  let filePath;
+  // HOME
+  if (req.url === '/') {
+    filePath = path.join(__dirname, 'src', 'index.html');
+    res.statusCode = 200;
+  } else if (req.url === '/about') {
+    filePath = path.join(__dirname, 'src','about.html');
+    res.statusCode = 200;
+  } else if (req.url === '/contact-me') {
+    filePath = path.join(__dirname, 'src', 'contact-me.html')
+    res.statusCode = 200;
+  } else {
+    filePath = path.join(__dirname, 'src', '404.html');
+    res.statusCode = 404;
+  }
+
+  // Sends an HTML file
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end()
+    } else {
+      res.end(data);
+    }
+  })
+})
+
+// localhost:8080
+server.listen(port, hostname, () => {
+  console.log('Sever is listening for requests on port 8080');
+});
